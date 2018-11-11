bf7e0ed8c05ef4807514f1c58753b19c15c845c4
Prahlad Misra <mprahlad95@gmail.com>
2018-11-11 14:50:56 -0500

    material toolbar and form added

--- mean-project/src/app/app.component.spec.ts
+++ mean-project/src/app/app.component.spec.ts
@@ -1,14 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
+        PostCreateComponent,
         AppComponent
       ],
     }).compileComponents();
