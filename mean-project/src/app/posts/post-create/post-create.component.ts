d0b9ff9922164a995d7b55fed3b94fd52162e66a
Prahlad Misra <mprahlad95@gmail.com>
2018-12-18 03:27:13 -0500

    posts added

--- mean-project/src/app/posts/post-create/post-create.component.ts
+++ mean-project/src/app/posts/post-create/post-create.component.ts
@@ -1,13 +1,13 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
+
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
--- mean-project/src/app/posts/post-list/post-list.component.ts
+++ mean-project/src/app/posts/post-list/post-list.component.ts
@@ -7,12 +7,11 @@ import { Post } from '../post.model';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-
 export class PostListComponent {
   // posts = [
-  //   { title: 'First Post', content: 'This is the first post\'s content'},
-  //   { title: 'Second Post', content: 'This is the second post\'s content'},
-  //   { title: 'Third Post', content: 'This is the third post\'s content'}
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
   // ];
   @Input() posts: Post[] = [];
 }
